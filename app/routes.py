d9904c0d6c71007e1ea07d627b800313939a0b44
Clay McGinnis <mcginnis@claymav.com>
2018-12-01 09:24:56 -0600

    Now runs

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
