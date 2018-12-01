ee441f3d697ee7dfe081d865e67c2ac2a867ae04
Katie Pitz <32210874+kppitz@users.noreply.github.com>
2018-12-01 14:55:30 -0600

    routing logic in place

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, render_template, url_for, request, redirect, session
-from flask import Flask, flash
+from flask import Flask, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,23 +9,39 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
+    if request.method == 'POST':
+        event = {
+            'event_name': request.form['eventName'],
+            'start_date': request.form['startTime'],
+            'end_date': request.form['endTime']
+        }
+        #add event to table
+        redirect_url = '/'+event['event_name']+"-"+event['startDate']
+        return redirect(redirect_url, code=302)
+    if request.method == 'GET':
+        #get all events
         return render_template('listview.html')
 
-@app.route('/event')
-def event():
+@app.route('/<event>', methods = ['POST', 'GET'])
+def event_route():
     if not session.get('logged_in'):
+        #drunk view
+        if request.method == 'POST':
+            #sos
+        if request.method == 'GET':
+        #get DDs from event, dd_list = get_dd(event)
         return render_template('drunkview.html')
     else:
+        #dd view
+        if request.method == 'POST':
+            #add driver to event
+        if request.method == 'GET':
+            #get
         return render_template('ddview.html')
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+
 
 
 @app.errorhandler(404)
