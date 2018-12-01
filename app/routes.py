ce59b9e06b2834bf1285dd303cc9759cb4bd66ae
Katie Pitz <32210874+kppitz@users.noreply.github.com>
2018-12-01 12:17:34 -0600

    login page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect, session
+from flask import Flask, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,10 +10,26 @@ db = SQLAlchemy(app)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
-
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello World!"
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['user']
+        password = request.form['password']
+        if  username == 'DD' and password == 'DD':
+            session['logged_in'] = True
+            return redirect('/', code=302)
+        else:
+            flash("Invalid login, try again")
+            return redirect('/login', code=302)
