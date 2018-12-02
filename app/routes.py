867a0b8992caa8a819092f3397b9a70938e17575
Katie Pitz <32210874+kppitz@users.noreply.github.com>
2018-12-01 20:35:37 -0600

    changed screen colors

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,8 @@ import os
 from flask import (Flask, abort, flash, redirect, render_template, request,
                    session)
 from flask_sqlalchemy import SQLAlchemy
-from twilio.rest import Client
-from twilio.twiml.voice_response import Say, VoiceResponse
+#from twilio.rest import Client
+#from twilio.twiml.voice_response import Say, VoiceResponse
 
 from app import app
 from app.utils import (add_driver_to_event, add_entry_to_db, auth_user,
@@ -36,9 +36,9 @@ def event_route(event):
         if request.method == 'POST':
             #sos
             # nums = ['+13145876003', '+13146517436']
-            nums = ['+13145876003']
-            for num in nums:
-                call(num)
+            #nums = ['+13145876003']
+            #for num in nums:
+            #    call(num)
             return redirect("/sos", code=302)
         if request.method == 'GET':
             #get DDs from event, dd_list = get_dd(event)
@@ -67,19 +67,19 @@ def page_not_found(error):
     return render_template('404.html')
 
 
-def call(num):
+#def call(num):
     # Your Account Sid and Auth Token from twilio.com/console
-    account_sid = 'AC4b362744f0815718c1a3159ddaeeccf4'
-    auth_token = 'c6f53fd9be6781e26005e93f7d1de239'
-    client = Client(account_sid, auth_token)
+    #account_sid = 'AC4b362744f0815718c1a3159ddaeeccf4'
+    #auth_token = 'c6f53fd9be6781e26005e93f7d1de239'
+    #client = Client(account_sid, auth_token)
 
-    call = client.calls.create(
-        url=
-        'https://handler.twilio.com/twiml/EH3d3693b942c9740bae7ec0a24fef443c',
-        to=num,
-        from_='+16364342737')
+    #call = client.calls.create(
+    #    url=
+    #    'https://handler.twilio.com/twiml/EH3d3693b942c9740bae7ec0a24fef443c',
+    #    to=num,
+    #    from_='+16364342737')
 
-    print(call.sid)
+    #print(call.sid)
 
 
 @app.route('/login', methods=['POST', 'GET'])
